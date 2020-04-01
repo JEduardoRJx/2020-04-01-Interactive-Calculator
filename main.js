1ffb23c9c0d374ed8fe3c789a25390ae719d630e
Eduardo <jeduardorjx@gmail.com>
2020-04-01 11:55:55 -0600

    Query select my inputs and buttons

---
+++ main.js
@@ -0,0 +1,5 @@
+let billAmount = document.querySelector('.billAmount');
+let numberOfPeople = document.querySelector('.numOfPeople');
+let serviceRating = document.querySelector('select').value;
+let calcBtn = document.querySelector('.calc-btn');
+let clearBtn = document.querySelector('.clear-btn')
