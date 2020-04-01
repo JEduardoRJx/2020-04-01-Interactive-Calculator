f8254d9455d3e06a84762083bd99f8d4c2c09b42
Eduardo <jeduardorjx@gmail.com>
2020-04-01 12:45:16 -0600

    Render the tip amount on the screen

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-let billAmount = document.querySelector('.bill-amount');
-let numOfPeople = document.querySelector('.num-of-people');
-let serviceRating = document.querySelector('select').value;
+let billAmountInput = document.querySelector('.bill-amount');
+let numOfPeopleInput = document.querySelector('.num-of-people');
+
 let calcBtn = document.querySelector('.calc-btn');
 let clearBtn = document.querySelector('.clear-btn')
 
 window.addEventListener('load', handlePageLoad)
+billAmountInput.addEventListener('keyup', btnStatus)
+numOfPeopleInput.addEventListener('keyup', btnStatus)
+
+calcBtn.addEventListener('click', calculateTip)
 
 function handlePageLoad() {
   // disable the buttons
@@ -12,11 +16,35 @@ function handlePageLoad() {
 }
 
 function btnStatus() {
-  if (billAmount.value === '' || numOfPeople.value === '') {
+  if (billAmountInput.value === '' || numOfPeopleInput.value === '') {
     calcBtn.disabled = true;
     clearBtn.disabled = true;
   } else {
     calcBtn.disabled = false;
     clearBtn.disabled = false;
   }
+}
+
+function calculateTip() {
+  // btnStatus();
+  let serviceRating = document.querySelector('select').value;
+  let billAmount = billAmountInput.value;
+  let numOfPeople = numOfPeopleInput.value;
+  let tipAmount = (billAmount * serviceRating).toFixed(2)
+  tipAmount = parseFloat(tipAmount)
+  let splitBillAmount = (billAmount / numOfPeople).toFixed(2)
+  splitBillAmount = parseFloat(splitBillAmount)
+  let total = tipAmount + splitBillAmount
+  renderAmount(total)
+}
+
+function renderAmount(total) {
+  console.log(total)
+  let body = document.querySelector('body');
+  body.insertAdjacentHTML('beforeend', `
+  <div class='total'> 
+    <h1 class='total'>TOTAL AMOUNT</h1>
+    <h1 class='total'>$${total} Each</h1>
+  </div>
+  `)
 }
