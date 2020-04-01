2b8524b61d0f072e1476731b0fe679ff67b35146
Eduardo <jeduardorjx@gmail.com>
2020-04-01 12:02:14 -0600

    Add event listener to the window to hear on page load

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ let numberOfPeople = document.querySelector('.numOfPeople');
 let serviceRating = document.querySelector('select').value;
 let calcBtn = document.querySelector('.calc-btn');
 let clearBtn = document.querySelector('.clear-btn')
+
+window.addEventListener('load', handlePageLoad)
+
+function handlePageLoad() {
+  // disable the buttons
+  disableBtns()
+}
